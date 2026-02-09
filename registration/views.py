69c1d0d58a27ab245f207a94aacac36aedda656a
adityabajra <adityabajracharya18@gmail.com>
2026-02-09 09:16:00 +0545

    hashing pass and form validation

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Registration
+from django.contrib.auth.hashers import make_password
 
 
 def registration_form(request):
@@ -10,10 +11,18 @@ def registration_form(request):
 
         if form.is_valid():
             # Get cleaned data
+            data = form.cleaned_data
+            hashPass = make_password(data['password'])
+            registration = Registration(
+                name=data['name'],
+                email=data['email'],
+                password=hashPass,  
+            )
+            registration.save()
             print("success")
             return render(request, 'registration/form.html',
                           {
-                              'form':form,
+                              'form':RegistrationForm(),
                               "success":"Successful"
                           })
     else:
