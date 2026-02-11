9cff77180583a9066ab3a86acc8a99f0896cede2
adityabajra <adityabajracharya18@gmail.com>
2026-02-11 09:04:42 +0545

    middleware

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'config.middleware.request_timing_middleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
