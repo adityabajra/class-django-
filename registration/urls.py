4c9541534511342660782251eaa4c8b1dd83266f
adityabajra <adityabajracharya18@gmail.com>
2026-02-25 09:45:35 +0545

    secure learning

--- registration/urls.py
+++ registration/urls.py
@@ -5,5 +5,6 @@ app_name = 'registration'
 
 urlpatterns = [
     path('',views.list_user, name='home'),
-    path('form/',views.registration_form, name='form'),
+    # path('form/',views.registration_form, name='form'),
+    path('form/',views.registration_form_sql_injection, name='form'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,13 +1,44 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Registration
 from django.contrib.auth.hashers import make_password
+from django.db import connection
 
 def list_user(request):
     users = Registration.objects.all()
     user_input = "<script>alert(`You are hacked. Stealing cookies. ${document.cookie}`);</script>"
     return render(request, 'registration/index.html', {'users': users , 
                                                        "user_input": user_input})
+
+@csrf_exempt
+def registration_form_sql_injection(request):
+    """Handle registration form display and submission"""
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            # Get cleaned data
+            data = form.cleaned_data
+            hashPass = make_password(data['password'])
+            
+            sql = f"""
+                INSERT INTO registration_registration (name, email, password)
+                VALUES ('{data['name']}', '{data['email']}', '{hashPass}');
+                """
+            
+            with connection.cursor() as cursor:
+                cursor.executescript(sql)
+            print("success")
+            return render(request, 'registration/form.html',
+                          {
+                              'form':RegistrationForm(),
+                              "success":"Successful"
+                          })
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration/form.html', {'form': form})
 
 
 
